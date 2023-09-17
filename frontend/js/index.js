6b9ea3b4156f960240098b496c64ba5d077ab73e
AbiyuTolosa <abiyunigussie7@gmail.com>
2023-09-17 20:45:59 +0300

    slider added using vanila javascript. event cards included at the bottom.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -12,3 +12,28 @@ closeIcon.addEventListener("click", () => {
     sideBar.style.display = "none";
   }
 });
+
+const backgroundImageClasses = [
+  "bg-one",
+  "bg-two",
+  "bg-three",
+  "bg-four",
+  "bg-five",
+];
+let currentIndex = 0;
+const slider = document.querySelector(".slider");
+
+function changeBackground() {
+  // Change the background image
+  const nextBackgroundIndex =
+    (currentIndex + 1) % backgroundImageClasses.length;
+
+  slider.classList.remove(backgroundImageClasses[currentIndex]);
+  slider.classList.add(backgroundImageClasses[nextBackgroundIndex]);
+
+  // Increment the index or reset to 0 if at the end
+  currentIndex = nextBackgroundIndex;
+}
+
+// Call the changeBackground function at an interval (e.g., every 5 seconds)
+setInterval(changeBackground, 5000); // 5000 milliseconds (5 seconds)
